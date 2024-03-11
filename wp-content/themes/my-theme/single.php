2fd4370dc5eefbc6ba589fced2c68ab4831a81b2
Sajed Ahmed <sajedaiub@gmail.com>
2024-03-12 03:43:25 +0600

    slider update

--- wp-content/themes/my-theme/single.php
+++ wp-content/themes/my-theme/single.php
@@ -5,7 +5,7 @@
             <?php while ( have_posts() ) : the_post(); ?>
                 <div class="main-post-div">
                 <div class="single-page-post-heading">
-                <h1><?php the_title(); ?></h1>
+                <h1><?php //the_title(); ?></h1>
                 </div>
                 <div class="content-here">
                 <?php  the_content();  ?>
